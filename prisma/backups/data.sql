c35c4c7b04061e03cecb0d0733332cc5bbf23335
sisqodataclub <sisqodataclub@users.noreply.github.com>
2025-04-11 18:54:37 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -1062,6 +1062,148 @@ COPY "pgsodium"."key" ("id", "status", "created", "expires", "key_type", "key_id
 
 
 --
+-- Data for Name: auth_group; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY "public"."auth_group" ("id", "name") FROM stdin;
+\.
+
+
+--
+-- Data for Name: django_content_type; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY "public"."django_content_type" ("id", "app_label", "model") FROM stdin;
+1	admin	logentry
+2	auth	permission
+3	auth	group
+4	auth	user
+5	contenttypes	contenttype
+6	sessions	session
+7	backend	todo
+\.
+
+
+--
+-- Data for Name: auth_permission; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY "public"."auth_permission" ("id", "name", "content_type_id", "codename") FROM stdin;
+1	Can add log entry	1	add_logentry
+2	Can change log entry	1	change_logentry
+3	Can delete log entry	1	delete_logentry
+4	Can view log entry	1	view_logentry
+5	Can add permission	2	add_permission
+6	Can change permission	2	change_permission
+7	Can delete permission	2	delete_permission
+8	Can view permission	2	view_permission
+9	Can add group	3	add_group
+10	Can change group	3	change_group
+11	Can delete group	3	delete_group
+12	Can view group	3	view_group
+13	Can add user	4	add_user
+14	Can change user	4	change_user
+15	Can delete user	4	delete_user
+16	Can view user	4	view_user
+17	Can add content type	5	add_contenttype
+18	Can change content type	5	change_contenttype
+19	Can delete content type	5	delete_contenttype
+20	Can view content type	5	view_contenttype
+21	Can add session	6	add_session
+22	Can change session	6	change_session
+23	Can delete session	6	delete_session
+24	Can view session	6	view_session
+25	Can add todo	7	add_todo
+26	Can change todo	7	change_todo
+27	Can delete todo	7	delete_todo
+28	Can view todo	7	view_todo
+\.
+
+
+--
+-- Data for Name: auth_group_permissions; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY "public"."auth_group_permissions" ("id", "group_id", "permission_id") FROM stdin;
+\.
+
+
+--
+-- Data for Name: auth_user; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY "public"."auth_user" ("id", "password", "last_login", "is_superuser", "username", "first_name", "last_name", "email", "is_staff", "is_active", "date_joined") FROM stdin;
+\.
+
+
+--
+-- Data for Name: auth_user_groups; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY "public"."auth_user_groups" ("id", "user_id", "group_id") FROM stdin;
+\.
+
+
+--
+-- Data for Name: auth_user_user_permissions; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY "public"."auth_user_user_permissions" ("id", "user_id", "permission_id") FROM stdin;
+\.
+
+
+--
+-- Data for Name: backend_todo; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY "public"."backend_todo" ("id", "title", "description", "completed") FROM stdin;
+\.
+
+
+--
+-- Data for Name: django_admin_log; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY "public"."django_admin_log" ("id", "action_time", "object_id", "object_repr", "action_flag", "change_message", "content_type_id", "user_id") FROM stdin;
+\.
+
+
+--
+-- Data for Name: django_migrations; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY "public"."django_migrations" ("id", "app", "name", "applied") FROM stdin;
+1	contenttypes	0001_initial	2025-04-11 16:04:16.436622+00
+2	auth	0001_initial	2025-04-11 16:04:22.172671+00
+3	admin	0001_initial	2025-04-11 16:04:23.53333+00
+4	admin	0002_logentry_remove_auto_add	2025-04-11 16:04:23.825627+00
+5	admin	0003_logentry_add_action_flag_choices	2025-04-11 16:04:24.346216+00
+6	contenttypes	0002_remove_content_type_name	2025-04-11 16:04:25.357559+00
+7	auth	0002_alter_permission_name_max_length	2025-04-11 16:04:25.773931+00
+8	auth	0003_alter_user_email_max_length	2025-04-11 16:04:26.314776+00
+9	auth	0004_alter_user_username_opts	2025-04-11 16:04:26.738789+00
+10	auth	0005_alter_user_last_login_null	2025-04-11 16:04:27.717675+00
+11	auth	0006_require_contenttypes_0002	2025-04-11 16:04:28.193361+00
+12	auth	0007_alter_validators_add_error_messages	2025-04-11 16:04:28.756945+00
+13	auth	0008_alter_user_username_max_length	2025-04-11 16:04:29.74701+00
+14	auth	0009_alter_user_last_name_max_length	2025-04-11 16:04:30.524628+00
+15	auth	0010_alter_group_name_max_length	2025-04-11 16:04:31.58686+00
+16	auth	0011_update_proxy_permissions	2025-04-11 16:04:32.008836+00
+17	auth	0012_alter_user_first_name_max_length	2025-04-11 16:04:32.763375+00
+18	backend	0001_initial	2025-04-11 16:04:33.84195+00
+19	sessions	0001_initial	2025-04-11 16:04:34.762299+00
+\.
+
+
+--
+-- Data for Name: django_session; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY "public"."django_session" ("session_key", "session_data", "expire_date") FROM stdin;
+\.
+
+
+--
 -- Data for Name: properties; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -3514,6 +3656,76 @@ SELECT pg_catalog.setval('"bronze"."rightmove_data_brz_id_seq"', 904, true);
 --
 
 SELECT pg_catalog.setval('"pgsodium"."key_key_id_seq"', 1, false);
+
+
+--
+-- Name: auth_group_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('"public"."auth_group_id_seq"', 1, false);
+
+
+--
+-- Name: auth_group_permissions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('"public"."auth_group_permissions_id_seq"', 1, false);
+
+
+--
+-- Name: auth_permission_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('"public"."auth_permission_id_seq"', 28, true);
+
+
+--
+-- Name: auth_user_groups_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('"public"."auth_user_groups_id_seq"', 1, false);
+
+
+--
+-- Name: auth_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('"public"."auth_user_id_seq"', 1, false);
+
+
+--
+-- Name: auth_user_user_permissions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('"public"."auth_user_user_permissions_id_seq"', 1, false);
+
+
+--
+-- Name: backend_todo_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('"public"."backend_todo_id_seq"', 1, false);
+
+
+--
+-- Name: django_admin_log_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('"public"."django_admin_log_id_seq"', 1, false);
+
+
+--
+-- Name: django_content_type_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('"public"."django_content_type_id_seq"', 7, true);
+
+
+--
+-- Name: django_migrations_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('"public"."django_migrations_id_seq"', 19, true);
 
 
 --
