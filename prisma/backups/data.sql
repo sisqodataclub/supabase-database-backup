0985db79299c68471006485cbeae33fe96209415
sisqodataclub <sisqodataclub@users.noreply.github.com>
2026-08-09 19:20:54 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict 9ktR2wLMsBcgpdTPP1Z8XJNORfwfP8LFmAsAGmgdXUUzL4nlwxlCfParLr9KGaD
+-- \restrict m9nUUgLpeVEBVwNwPaSlM6rQsVydhB6c6yreOPTgPa1YLxXsc5tQa6VGckVuLPg
 
 -- Dumped from database version 15.8
 -- Dumped by pg_dump version 17.6
@@ -3861,6 +3861,6 @@ SELECT pg_catalog.setval('"silver"."properties_df_sil_id_seq"', 1, false);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict 9ktR2wLMsBcgpdTPP1Z8XJNORfwfP8LFmAsAGmgdXUUzL4nlwxlCfParLr9KGaD
+-- \unrestrict m9nUUgLpeVEBVwNwPaSlM6rQsVydhB6c6yreOPTgPa1YLxXsc5tQa6VGckVuLPg
 
 RESET ALL;
